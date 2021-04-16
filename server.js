171ffd849720e5d10f22b8e2c077cbd6028c42bc
James Quillin <jamesquillin1@gmail.com>
2021-04-15 22:50:01 -0700

    update port to 80

--- server.js
+++ server.js
@@ -213,6 +213,6 @@ io.on('connection', socket => {
   });
 });
 
-server.listen(3000, () => {
-  console.log('server running on port 3000');
+server.listen(80, () => {
+  console.log('server running on port 80');
 });
